0355d423208bfedef49ca1a5262d72dd77e32273
Jason Wang <wanghung07@gmail.com>
2022-12-09 09:59:08 +0800

    remove target file

--- ch3/variables/target/debug/deps/variables-af29bafb44e2cff0.d
+++
@@ -1,5 +0,0 @@
-/Users/jasonwang/Desktop/rust-playground/ch3/variables/target/debug/deps/variables-af29bafb44e2cff0.rmeta: src/main.rs
-
-/Users/jasonwang/Desktop/rust-playground/ch3/variables/target/debug/deps/variables-af29bafb44e2cff0.d: src/main.rs
-
-src/main.rs:
--- ch3/variables/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-/Users/jasonwang/Desktop/rust-playground/ch3/variables/target/debug/variables: /Users/jasonwang/Desktop/rust-playground/ch3/variables/src/main.rs
