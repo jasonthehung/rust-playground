2070e4057a14aada06e80a266f98547d28a624ef
Jason Wang <wanghung07@gmail.com>
2022-12-11 02:27:55 +0800

    ch3 functions

--- ch3/functions/src/main.rs
+++ ch3/functions/src/main.rs
@@ -3,9 +3,23 @@
 fn main() {
     println!("Hello, world!");
 
-    other_function();
+    let x = {
+        let y = 1;
+        y + 1
+    };
+
+    println!("x is: {x}");
+
+    let five = five();
+    println!("Calling function five is : {five}");
+
+    other_function(5);
 }
 
-fn other_function() {
-    println!("Jason's here");
+fn other_function(x: u32) {
+    println!("Jason's here {x}");
+}
+
+fn five() -> i32 {
+    -5
 }
