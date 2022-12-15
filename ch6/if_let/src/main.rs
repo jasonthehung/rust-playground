e2b0a9f1a6a4e32b440c097d90ddd9f6d29270d0
Jason Wang <wanghung07@gmail.com>
2022-12-15 16:54:48 +0800

    6-3

---
+++ ch6/if_let/src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    // ! if let lose the exhaustive checking that match enforces
+    // let config_max = Some(3u8);
+    // match config_max {
+    //     Some(max) => println!("The maximum is configured to be {}", max),
+    //     _ => (),
+    // }
+    // @ Same
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
+    }
+
+    // let mut count = 0;
+    // match coin {
+    //     Coin::Quarter(state) => println!("State quarter from {:?}!", state),
+    //     _ => count += 1,
+    // }
+    // @ Same
+    // let mut count = 0;
+    // if let Coin::Quarter(state) = coin {
+    //     println!("State quarter from {:?}!", state);
+    // } else {
+    //     count += 1;
+    // }
+}
