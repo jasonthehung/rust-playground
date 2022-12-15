f00d3f2086e5325a79416d29ce93c133a9a031aa
Jason Wang <wanghung07@gmail.com>
2022-12-15 16:40:48 +0800

    6-2

---
+++ ch6/match_control_flow_construct/src/main.rs
@@ -0,0 +1,49 @@
+fn main() {
+    println!("Hello, world!");
+
+    let res_1 = value_in_cents(Coin::Penny);
+    let res_2 = value_in_cents(Coin::Quarter(UsState::Alabama));
+
+    println!("{}", res_1);
+    println!("{}", res_2);
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}", state);
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
